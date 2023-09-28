47713728439d0b0efa7b4746fc99b762691676e5
Will Luna <willluna.zac.com@gmail.com>
2023-09-28 13:04:07 -0500

    Gestor de tareas

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fondo">
+    <Navbar />
+    <Tarea />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Tarea from "./components/Tarea.vue";
+export default {
+  name: "App",
+  components: { Navbar, Tarea },
+};
+</script>
+
+<style>
+.fondo {
+  background-image: url("./assets/fondo.jpg");
+  height: 100vh;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">App Tareas</span>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
